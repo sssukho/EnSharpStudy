62669fe8fdcd3527cb44cf429b03183b0700f50b
SUKHOLIM6D05\sukholim <sssukho@gmail.com>
2018-05-10 21:33:55 +0900

    이미지 검색 텍스트 예외처리 완료

--- ImageFind/ImageSearch/ImageSearch/ErrorCheck.cs
+++ ImageFind/ImageSearch/ImageSearch/ErrorCheck.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
 
 namespace ImageSearch
 {
@@ -19,10 +20,26 @@ namespace ImageSearch
 
         public bool IsValidSearch(string searchWord)
         {
-            if (searchWord.Length > 50)
+
+            if (searchWord.Length > 50 || string.IsNullOrEmpty(searchWord) || string.IsNullOrWhiteSpace(searchWord))
                 return false;
 
             return true;
+        }
+
+        public bool IsValidStatusCode(System.Exception err)
+        {
+            if (err is WebException)
+            {
+                WebException we = (WebException)err;
+                if (we.Response is HttpWebResponse)
+                {
+                    HttpWebResponse response = (HttpWebResponse)we.Response;
+                    if (response.StatusCode.ToString().Contains("404"))
+                        return true;
+                }
+            }
+            return false;
         }
     }
 }
