8b518323c77506303da507acb29690fafbf3bd78
SUKHOLIM6D05\sukholim <sssukho@gmail.com>
2018-05-11 03:44:29 +0900

    VO 클래스 속성값 get set 완료

--- LibraryManagement/LibraryManagement/VO/BookVO.cs
+++ LibraryManagement/LibraryManagement/VO/BookVO.cs
@@ -8,5 +8,73 @@ namespace LibraryManagement
 {
     class BookVO
     {
+        private string name;
+        private string author;
+        private string price;
+        private string publisher;
+        private string publishDate;
+        private int count;
+        private string isbn;
+        private string explanation;
+
+        public BookVO(string name, string author, string price, string publisher, string publishDate, int count, string isbn, string explanation)
+        {
+            this.name = name;
+            this.author = author;
+            this.price = price;
+            this.publisher = publisher;
+            this.publishDate = publishDate;
+            this.count = count;
+            this.isbn = isbn;
+            this.explanation = explanation;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+
+        public string Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public string Publisher
+        {
+            get { return publisher; }
+            set { publisher = value; }
+        }
+
+        public string PublishDate
+        {
+            get { return publishDate; }
+            set { publishDate = value; }
+        }
+
+        public int Count
+        {
+            get { return count; }
+            set { count = value; }
+        }
+
+        public string Isbn
+        {
+            get { return isbn; }
+            set { isbn = value; }
+        }
+
+        public string Explanation
+        {
+            get { return explanation; }
+            set { explanation = value; }
+        }
     }
 }
--- LibraryManagement/LibraryManagement/VO/MemberVO.cs
+++ LibraryManagement/LibraryManagement/VO/MemberVO.cs
@@ -8,5 +8,88 @@ namespace LibraryManagement
 {
     class MemberVO
     {
+        string id;
+        string password;
+        string name;
+        string gender;
+        string phoneNumber;
+        string email;
+        string address;
+        string rentBook;
+        string dueDate;
+        int extensionCount;
+
+        public MemberVO(string id, string password, string name, string gender, string phoneNumber, string email, string address, string rentBook, string dueDate, int extensionCount)
+        {
+            this.id = id;
+            this.password = password;
+            this.name = name;
+            this.gender = gender;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+            this.address = address;
+            this.rentBook = rentBook;
+            this.dueDate = dueDate;
+            this.extensionCount = extensionCount;
+        }
+
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { id = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Gender
+        {
+            get { return gender; }
+            set { gender = value; }
+        }
+
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+            set { phoneNumber = value; }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        public string RentBook
+        {
+            get { return rentBook; }
+            set { rentBook = value; }
+        }
+
+        public string DueDate
+        {
+            get { return dueDate; }
+            set { dueDate = value; }
+        }
+
+        public int ExtensionCount
+        {
+            get { return extensionCount; }
+            set { extensionCount = value; }
+        }
     }
 }
