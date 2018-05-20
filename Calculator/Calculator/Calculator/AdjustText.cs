7d90546f1571eaaf020fe6dcaefb0e6ccd315a54
SUKHOLIM6D05\sukholim <sssukho@gmail.com>
2018-05-20 19:50:46 +0900

    천단위 콤마 추가 기능과 폰트 사이즈 조정 메소드가 있는 AdjustText 클래스 추가 완료.

---
+++ Calculator/Calculator/Calculator/AdjustText.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class AdjustText
+    {
+        public AdjustText() { }
+
+        public string AddComma(string inputText)
+        {
+            if(inputText.Contains("E")) //자연상수
+                return inputText;
+
+            if (inputText.Equals("0"))
+                return inputText;
+            
+            inputText = inputText.Replace(",", "");
+            inputText = string.Format("{0:#,###,###,###,###,###}", Convert.ToDouble(inputText));
+            return inputText;
+        }
+
+        public int AdjustFontSize(string inputText)
+        {
+            switch (inputText.Length)
+            {
+                case 21:
+                    return 39;
+
+                case 20:
+                    return 42;
+
+                case 19:
+                    return 45;
+
+                case 18:
+                    return 48;
+
+                case 17:
+                    return 51;
+
+                case 16:
+                    return 54;
+
+                case 15:
+                    return 57;
+            }
+            return 60;
+        }
+    }
+}
