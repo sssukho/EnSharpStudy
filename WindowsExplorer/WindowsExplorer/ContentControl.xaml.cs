b55e5f0b64cedce0e563aef388198b3abef754ae
SUKHOLIM6D05\sukholim <sssukho@gmail.com>
2018-06-01 18:50:54 +0900

    클래스 구성 완료

--- WindowsExplorer/WindowsExplorer/ContentControl.xaml.cs
+++ WindowsExplorer/WindowsExplorer/ContentControl.xaml.cs
@@ -16,14 +16,13 @@ using System.Windows.Shapes;
 namespace WindowsExplorer
 {
     /// <summary>
-    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// ContentControl.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ContentControl : UserControl
     {
-        public MainWindow()
+        public ContentControl()
         {
             InitializeComponent();
-            
         }
     }
 }
--- WindowsExplorer/WindowsExplorer/TreeStructureControl.xaml.cs
+++ WindowsExplorer/WindowsExplorer/TreeStructureControl.xaml.cs
@@ -16,14 +16,13 @@ using System.Windows.Shapes;
 namespace WindowsExplorer
 {
     /// <summary>
-    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// TreeStructureControl.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class TreeStructureControl : UserControl
     {
-        public MainWindow()
+        public TreeStructureControl()
         {
             InitializeComponent();
-            
         }
     }
 }
