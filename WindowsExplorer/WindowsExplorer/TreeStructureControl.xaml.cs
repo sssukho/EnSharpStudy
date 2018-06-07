39f3ceeab3548df732c8c11060cb7d5b998c2ac5
SUKHOLIM6D05\sukholim <sssukho@gmail.com>
2018-06-08 02:23:05 +0900

    CD ROM 드라이브 예외처리

--- WindowsExplorer/WindowsExplorer/TreeStructureControl.xaml.cs
+++ WindowsExplorer/WindowsExplorer/TreeStructureControl.xaml.cs
@@ -7,11 +7,13 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Media;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace WindowsExplorer
 {
@@ -23,6 +25,50 @@ namespace WindowsExplorer
         public TreeStructureControl()
         {
             InitializeComponent();
+            InitializeView();
+        }
+
+        public void InitializeView()
+        {
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            
+            foreach (DriveInfo drive in allDrives)
+            {
+                if(drive.IsReady)
+                {
+                    TreeViewItem treeViewItem = new TreeViewItem
+                    {
+                        Header = drive,
+                        Tag = drive
+                    };
+                    treeView.Items.Add(treeViewItem);
+                    treeViewItem.FontWeight = FontWeights.Normal;
+
+                    treeViewItem.Expanded += new RoutedEventHandler(FolderExpanded);
+                }
+            }
+        }
+
+        public void FolderExpanded(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem item = (TreeViewItem)sender;
+            
+            item.Items.Clear();
+            foreach (string directory in Directory.GetDirectories(item.Tag.ToString()))
+            {
+                TreeViewItem subitem = new TreeViewItem();
+                subitem.Header = directory.Substring(directory.LastIndexOf("\\") + 1);
+                subitem.Tag = directory;
+                subitem.FontWeight = FontWeights.Normal;
+                subitem.Expanded += new RoutedEventHandler(TreeViewItemSelected);
+                item.Items.Add(subitem);
+            }
+
+        }
+
+        public void TreeViewItemSelected(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
